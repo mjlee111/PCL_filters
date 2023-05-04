879250ee7f880d4fe58f4d4265781bd9418efbb7
Myeung Jin Lee <66550892+mjlee111@users.noreply.github.com>
2023-05-04 22:40:48 +0900

    Add files via upload

--- src/PCL_filters.cpp
+++ src/PCL_filters.cpp
@@ -13,7 +13,10 @@ void passthrough_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg,
 void voxel_grid_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg, float x_leaf, float y_leaf, float z_leaf);
 void outliner_removal_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg, int meanK, float threshold);
 
-void three_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg);
+void three_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg
+                  , float x_min, float x_max, float y_min, float y_max, float z_min, float z_max
+                  , float x_leaf, float y_leaf, float z_leaf
+                  , int meanK, float threshold);
 
 ros::Subscriber input_cloud_sub;
 
@@ -27,9 +30,9 @@ int main(int argc, char** argv){
   ros::NodeHandle n;
 
   input_cloud_sub = n.subscribe<sensor_msgs::PointCloud2>("/camera/depth/color/points", 1, filter_callback);
-  passthrough_cloud_pub = n.advertise<sensor_msgs::PointCloud2>("passthrough_cloud", 1);
-  voxel_cloud_pub = n.advertise<sensor_msgs::PointCloud2>("voxel_cloud", 1);
-  outliner_cloud_pub = n.advertise<sensor_msgs::PointCloud2>("outliner_cloud", 1);
+  //passthrough_cloud_pub = n.advertise<sensor_msgs::PointCloud2>("passthrough_cloud", 1);
+  //voxel_cloud_pub = n.advertise<sensor_msgs::PointCloud2>("voxel_cloud", 1);
+  //outliner_cloud_pub = n.advertise<sensor_msgs::PointCloud2>("outliner_cloud", 1);
   whole_cloud_pub = n.advertise<sensor_msgs::PointCloud2>("filtered_cloud", 1);
   ros::spin();
 
@@ -38,10 +41,10 @@ int main(int argc, char** argv){
 
 
 void filter_callback(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg){
-  passthrough_filter(input_cloud_msg, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0);
-  voxel_grid_filter(input_cloud_msg, 0.07, 0.07, 0.07);
-  outliner_removal_filter(input_cloud_msg, 50, 1.0);
-  three_filter(input_cloud_msg);
+  //passthrough_filter(input_cloud_msg, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0);
+  //voxel_grid_filter(input_cloud_msg, 0.07, 0.07, 0.07);
+  //outliner_removal_filter(input_cloud_msg, 50, 1.0);
+  three_filter(input_cloud_msg, 0.0, 2.0, 0.0, 2.0, 0.0, 2.0, 0.07, 0.07, 0.07, 50, 1.0);
 }
 
 void passthrough_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg, float x_min, float x_max, float y_min, float y_max, float z_min, float z_max){
@@ -100,18 +103,22 @@ void outliner_removal_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud
   outliner_cloud_pub.publish(output_cloud_msg);
 }
 
-void three_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg){
+void three_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg
+                  , float x_min, float x_max, float y_min, float y_max, float z_min, float z_max
+                  , float x_leaf, float y_leaf, float z_leaf
+                  , int meanK, float threshold){
+
   pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_passthrough(new pcl::PointCloud<pcl::PointXYZ>);
   pcl::fromROSMsg(*input_cloud_msg, *input_cloud_passthrough);
 
   pcl::PassThrough<pcl::PointXYZ> pass_filter;
   pass_filter.setInputCloud(input_cloud_passthrough);
   pass_filter.setFilterFieldName("x");
-  pass_filter.setFilterLimits(0.0, 2.0);
+  pass_filter.setFilterLimits(x_min, x_max);
   pass_filter.setFilterFieldName("y");
-  pass_filter.setFilterLimits(0.0, 2.0);
+  pass_filter.setFilterLimits(y_min, y_max);
   pass_filter.setFilterFieldName("z");
-  pass_filter.setFilterLimits(0.0, 2.0);
+  pass_filter.setFilterLimits(z_min, z_max);
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud_1(new pcl::PointCloud<pcl::PointXYZ>);
   pass_filter.filter(*filtered_cloud_1);
@@ -120,7 +127,7 @@ void three_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg){
 
   pcl::VoxelGrid<pcl::PointXYZ> voxel_filter;
   voxel_filter.setInputCloud(filtered_cloud_1);
-  voxel_filter.setLeafSize(0.07f, 0.07f, 0.07f);
+  voxel_filter.setLeafSize(x_leaf, y_leaf, z_leaf);
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud_2(new pcl::PointCloud<pcl::PointXYZ>);
   voxel_filter.filter(*filtered_cloud_2);
@@ -129,8 +136,8 @@ void three_filter(const sensor_msgs::PointCloud2ConstPtr& input_cloud_msg){
 
   pcl::StatisticalOutlierRemoval<pcl::PointXYZ> outliner_filter;
   outliner_filter.setInputCloud(filtered_cloud_2);
-  outliner_filter.setMeanK(50);
-  outliner_filter.setStddevMulThresh(1.0);
+  outliner_filter.setMeanK(meanK);
+  outliner_filter.setStddevMulThresh(threshold);
   pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud(new pcl::PointCloud<pcl::PointXYZ>);
   outliner_filter.filter(*output_cloud);
 
